99af1dd732a4de41d346fac24e7f06776552f9e7
Tiago Machado <tiagodjm@Tiagos-MacBook.local>
2016-03-26 02:37:22 +1000

    added files for ballon app

--- ballon/ViewController.swift
+++ ballon/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBallon: UIImageView!
+    @IBOutlet weak var redBallon: UIImageView!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var hideRedButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlueBallon(sender: AnyObject) {
+        
+        blueBallon.hidden = true
+        redBallon.hidden = false
+        
+    }
 
+    @IBAction func hideRedBallon(sender: AnyObject) {
+        
+        blueBallon.hidden = false
+        redBallon.hidden = true
+    }
 }
 
